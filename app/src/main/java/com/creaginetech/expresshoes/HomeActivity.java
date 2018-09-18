e20643360c6d49eb525c1c5e9f86edf2ff9a07cc
firlanaryu <firlanasanur@gmail.com>
2018-09-18 09:06:45 +0700

    xpreshoes baru

---
+++ app/src/main/java/com/creaginetech/expresshoes/HomeActivity.java
@@ -0,0 +1,392 @@
+package com.creaginetech.expresshoes;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.andremion.counterfab.CounterFab;
+import com.creaginetech.expresshoes.Common.Common;
+import com.creaginetech.expresshoes.Database.Database;
+import com.creaginetech.expresshoes.Interface.ItemClickListener;
+import com.creaginetech.expresshoes.Model.Category;
+import com.creaginetech.expresshoes.Model.Token;
+import com.creaginetech.expresshoes.ViewHolder.MenuViewHolder;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.rengwuxian.materialedittext.MaterialEditText;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import dmax.dialog.SpotsDialog;
+import io.paperdb.Paper;
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
+public class HomeActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    FirebaseDatabase database;
+    DatabaseReference category;
+
+    TextView txtFullName;
+
+    RecyclerView recycler_menu;
+    RecyclerView.LayoutManager layoutManager;
+
+    FirebaseRecyclerAdapter<Category,MenuViewHolder> adapter;
+
+    SwipeRefreshLayout swipeRefreshLayout;
+
+    CounterFab fab;
+
+    boolean doubleBackToExitPressedOnce = false;
+
+
+    //calligraphy
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Note: add this code before setContentView method
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/cf.otf")
+                .setFontAttrId(R.attr.fontPath)
+                .build());
+
+        setContentView(R.layout.activity_home);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Menu");
+        setSupportActionBar(toolbar);
+
+        //View - cp-28 (Refresh layout)
+        swipeRefreshLayout = (SwipeRefreshLayout)findViewById(R.id.swipe_layout);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary,
+                android.R.color.holo_green_dark,
+                android.R.color.holo_orange_dark,
+                android.R.color.holo_blue_dark);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                if(Common.isConnectedToInternet(getBaseContext())) //cp15 check intrnt
+                    loadMenu();
+                else {
+                    Toast.makeText(getBaseContext(), "Please Check your connection", Toast.LENGTH_SHORT).show(); //check internet connnection
+                    return;
+                }
+            }
+        });
+
+        //Default, load for first time
+        swipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                if(Common.isConnectedToInternet(getBaseContext())) //cp15 check intrnt
+                    loadMenu();
+                else {
+                    Toast.makeText(getBaseContext(), "Please Check your connection", Toast.LENGTH_SHORT).show(); //check internet connnection
+                    return;
+                }
+            }
+        });
+
+
+        //Init Firebase
+        database = FirebaseDatabase.getInstance();
+        category = database.getReference("Category");
+
+        Paper.init(this);
+
+        //Cart FAB
+        fab = (CounterFab) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Button add Cart
+                Intent cartIntent = new Intent(HomeActivity.this,CartActivity.class);
+                startActivity(cartIntent);
+            }
+        });
+        //count cart
+        fab.setCount(new Database(this).getCountCart());
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //Set Name for user
+        View headerView = navigationView.getHeaderView(0);
+        txtFullName = (TextView)headerView.findViewById(R.id.txtFullName);
+        txtFullName.setText(Common.currentUser.getName());
+
+        //Load menu
+        recycler_menu = (RecyclerView)findViewById(R.id.recycler_menu);
+
+        recycler_menu.setLayoutManager(new GridLayoutManager(this,2)); //grid layout halaman home
+
+
+        //to add your token when login app
+        updateToken(FirebaseInstanceId.getInstance().getToken());
+
+
+    }
+
+    private void updateToken(String token) {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        DatabaseReference tokens = db.getReference("Tokens");
+        Token data = new Token(token,false); //false because this token from client app
+        tokens.child(Common.currentUser.getPhone()).setValue(data);
+    }
+
+    private void loadMenu() {
+
+        FirebaseRecyclerOptions<Category> options = new FirebaseRecyclerOptions.Builder<Category>()
+                .setQuery(category,Category.class)
+                .build();
+
+        adapter = new FirebaseRecyclerAdapter<Category, MenuViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MenuViewHolder viewHolder, int position, @NonNull Category model) {
+                viewHolder.txtMenuName.setText(model.getName());
+                Picasso.with(getBaseContext()).load(model.getImage())
+                        .into(viewHolder.imageView);
+                final Category clickItem = model;
+                viewHolder.setItemClickListener(new ItemClickListener() {
+                    @Override
+                    public void onClick(View view, int position, boolean isLongClick) {
+                        //Get CategoryId and send to new Activity
+                        Intent foodList = new Intent(HomeActivity.this,FoodList.class);
+                        //Because CategoryId
+                        foodList.putExtra("CategoryId",adapter.getRef(position).getKey());
+                        startActivity(foodList);
+                    }
+                });
+            }
+
+            @Override
+            public MenuViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View itemView = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.menu_item,parent,false);
+                return new MenuViewHolder(itemView);
+            }
+        };
+
+
+        adapter.startListening();
+        recycler_menu.setAdapter(adapter);
+        swipeRefreshLayout.setRefreshing(false);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        fab.setCount(new Database(this).getCountCart());
+        //show category when click back button from food
+        if (adapter != null)
+            adapter.startListening();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+//            super.onBackPressed();
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed();
+                return;
+            }
+
+            this.doubleBackToExitPressedOnce = true;
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+            new Handler().postDelayed(new Runnable() {
+
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 2000);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.refresh)
+            loadMenu();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_menu) {
+
+        } else if (id == R.id.nav_cart) {
+            Intent cartIntent = new Intent(HomeActivity.this,CartActivity.class);
+            startActivity(cartIntent);
+
+        } else if (id == R.id.nav_orders) {
+            Intent orderIntent = new Intent(HomeActivity.this,OrderStatusActivity.class);
+            startActivity(orderIntent);
+
+        } else if (id == R.id.nav_log_out) {
+
+            //Delete remember user and password (delete all key value saved after press logout (exit app))
+            Paper.book().destroy();
+
+            //Logout
+            Intent signIn = new Intent(HomeActivity.this,SignInActivity.class);
+            signIn.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(signIn);
+
+        } else if (id == R.id.nav_change_password) {
+            showChangePasswordDialog();
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    private void showChangePasswordDialog() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(HomeActivity.this);
+        alertDialog.setTitle("CHANGE PASSWORD");
+        alertDialog.setMessage("Please fill all information !");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View layout_password = inflater.inflate(R.layout.change_password_layout,null);
+
+        final MaterialEditText edtPassword = (MaterialEditText)layout_password.findViewById(R.id.edtPassword);
+        final MaterialEditText edtNewPassword = (MaterialEditText)layout_password.findViewById(R.id.edtNewPassword);
+        final MaterialEditText edtRepeatPassword = (MaterialEditText)layout_password.findViewById(R.id.edtRepeatPassword);
+
+        alertDialog.setView(layout_password);
+
+        //Button
+        alertDialog.setPositiveButton("CHANGE", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //For use SpotsDialog, please use AlertDialog from android.app, not from v7 like above AlertDialog
+                final android.app.AlertDialog waitingDialog = new SpotsDialog(HomeActivity.this);
+                waitingDialog.show();
+
+                //Check old password
+                if (edtPassword.getText().toString().equals(Common.currentUser.getPassword()))
+                {
+                    //Check new password and repeat password
+                    if (edtNewPassword.getText().toString().equals(edtRepeatPassword.getText().toString()))
+                    {
+                        Map<String,Object> passwordUpdate = new HashMap<>();
+                        passwordUpdate.put("password",edtNewPassword.getText().toString());
+
+                        //Make Update
+                        DatabaseReference user = FirebaseDatabase.getInstance().getReference("User");
+                        user.child(Common.currentUser.getPhone())
+                                .updateChildren(passwordUpdate)
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        waitingDialog.dismiss();
+                                        Toast.makeText(HomeActivity.this, "Password was update", Toast.LENGTH_SHORT).show();
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Toast.makeText(HomeActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    } else {
+                        waitingDialog.dismiss();
+                        Toast.makeText(HomeActivity.this, "New Password doesn't match", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else
+                {
+                    waitingDialog.dismiss();
+                    Toast.makeText(HomeActivity.this, "Wrong old password !", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        alertDialog.show();
+
+    }
+}
